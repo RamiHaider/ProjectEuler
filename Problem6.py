09c477675621a0ff06303fb0739ba8e20e8d2d48
RamiHaider <ramiaboushamalah@gmail.com>
2024-07-25 20:32:06 -0400

    Solution added and complete

--- Problem6.py
+++ Problem6.py
@@ -1,3 +1,16 @@
 #Find the difference between the sum of the squares of the first one hundred
 #natural numbers and the square of the sums
 
+sum_Of_Squares = 0
+
+for i in range(1,101):    
+    sum_Of_Squares += i**2
+
+sum_Of_Naturals = 0
+for i in range(1,101):
+    sum_Of_Naturals += i    
+square_Of_Sums = sum_Of_Naturals**2
+
+Difference = square_Of_Sums - sum_Of_Squares
+
+print(f'The Square of the Sums is {square_Of_Sums} whilst the sum of the squares is {sum_Of_Squares}, thus, the difference is = {Difference}')
